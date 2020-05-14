2cf309a0db9dd108439632dbaebcd5ed63b013af
shandianchengzi <58585665+shandianchengzi@users.noreply.github.com>
2020-05-14 19:13:34 +0800

    Update index.js

--- task3/src/pages/login/index.js
+++ task3/src/pages/login/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Form, Input, Button } from 'antd';
+import { Link } from 'react-router-dom';
 /* 1.用于创建一个实体或收集信息。
 2.需要对输入的数据类型进行校验时。 */
 import './index.css'
-import APP from '../home/index.js'
+
 const layout = {
     labelCol: { span: 8 },
     wrapperCol: { span: 16 },
@@ -15,20 +16,19 @@ const tailLayout = {
     },
 };
 
-const Login = (props) => {/* Login是个值？ */
+const Login = () => {/* Login是个返回表单的函数 */
     const onFinish = values => {
         console.log('Success:', values);
-        props.onSubmit();/* onSubmit 事件会在表单中的确认按钮被点击时发生。 */
+        /* onSubmit 事件会在表单中的确认按钮被点击时发生。 */
     };/* onFinish让控制台在提交成功时输出Success:values? */
 
     const onFinishFailed = errorInfo => {
         console.log('Failed:', errorInfo);
     };/* onFinishFailed让控制台在提交失败时输出Failed:errorInfo? */
-
     return (/* return的了一整个表的功能 */
         <div className="container">{/* 建立表单盒子 */}
             <div className='title'>Team Blog</div>
-            <Form
+            <Form 
                 {...layout}
                 name="basic"
                 initialValues={{
@@ -36,6 +36,7 @@ const Login = (props) => {/* Login是个值？ */
                 }}
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}
+                onSubmit="this.props.history.push('\Home')"
             >{/* 第一个表格单元的构成 */}
                 <Form.Item
                     label="Username"
@@ -65,12 +66,14 @@ const Login = (props) => {/* Login是个值？ */
                     {/* 第三个表格单元的构成 */}
                 <Form.Item {...tailLayout}>{/* 使用tailLayout样式 */}
                     <Button type="primary" htmlType="submit" >{/* 按钮的样式类型，提交表单，提交啥玩意啊？ */}
-                        Submit
+                        <Link to="/Home">Submit</Link> 
                     </Button>
                 </Form.Item>
             </Form>
         </div>
     );
+    
 };
 /* 定义输出接口，以便在其他文件中，复用组件 */
 export default Login;
+    
