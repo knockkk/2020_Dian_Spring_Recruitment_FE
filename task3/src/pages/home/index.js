96cb14e26c82f475ffa8ff85341c077908198065
shandianchengzi <58585665+shandianchengzi@users.noreply.github.com>
2020-05-16 19:56:30 +0800

    Update index.js

--- task3/src/pages/home/index.js
+++ task3/src/pages/home/index.js
@@ -5,51 +5,30 @@ import { FileTextOutlined, EditOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom';
 const { Header, Content } = Layout;
 
-async function getDate(i){
+async function getDate(i) {
     const response = await fetch("https://5e9c0a2810bf9c0016dd2581.mockapi.io/api/articles");//它是异步的，需要等待它返回response(一个流)
     const posts = await response.json();//用json函数将它转换为json
 
-        const h2=document.createElement('H2');
-        const p=document.createElement('p');
-        const p2=document.createElement('p');
-        const img=document.createElement('img');
-        
-        h2.appendChild(document.createTextNode(posts[i].id));
-        p.appendChild(document.createTextNode(posts[i].createdAt));
-        p2.appendChild(document.createTextNode(posts[i].author));
-        if(i==0)
-        { document.querySelector('#title').appendChild(h2);
-        document.querySelector('#date').appendChild(p);
-        document.querySelector('#author').appendChild(p2);
-        img.src=posts[i].avatar;
-        document.querySelector('#avatar').appendChild(img);}
-        else if(i==1){
-            document.querySelector('#title2').appendChild(h2);
-        document.querySelector('#date2').appendChild(p);
-        document.querySelector('#author2').appendChild(p2);
-        img.src=posts[i].avatar;
-        document.querySelector('#avatar2').appendChild(img);
-        }
-        else if(i==2)
-        {
-            document.querySelector('#title3').appendChild(h2);
-        document.querySelector('#date3').appendChild(p);
-        document.querySelector('#author3').appendChild(p2);
-        img.src=posts[i].avatar;
-        document.querySelector('#avatar3').appendChild(img);
-        }
-        else if(i==3)
-        { document.querySelector('#title4').appendChild(h2);
-        document.querySelector('#date4').appendChild(p);
-        document.querySelector('#author4').appendChild(p2);
-        img.src=posts[i].avatar;
-        document.querySelector('#avatar4').appendChild(img);}
-        document.getElementsByClassName('emm')[i].style.display='block';
-        document.getElementsByClassName('loader')[i].style.display='none';
-        //怎么改昂，待我看看什么row之类的再来改吧。。
+    const h2 = document.createElement('H2');
+    const p = document.createElement('p');
+    const p2 = document.createElement('p');
+    const img = document.createElement('img');
+
+    h2.appendChild(document.createTextNode(posts[i].id));
+    p.appendChild(document.createTextNode(posts[i].createdAt));
+    p2.appendChild(document.createTextNode(posts[i].author));
+    document.getElementById(i).appendChild(h2);
+    document.getElementById(i + 4).appendChild(p);
+    document.getElementById(i + 8).appendChild(p2);
+    img.src = posts[i].avatar;
+    document.getElementById(i + 12).appendChild(img);
+    document.getElementsByClassName('emm')[i].style.display = 'block';
+    document.getElementsByClassName('loader')[i].style.display = 'none';
 }
-for(var i=0;i<4;i++)
-getDate(i);
+//怎么改昂，待我看看什么row之类的再来改吧。。
+
+for (var i = 0; i < 4; i++)
+    getDate(i);
 class Home extends React.Component {
     render() {
         return (<Layout>
@@ -63,44 +42,45 @@ class Home extends React.Component {
             </Menu>
             <Content><div className="Block">
                 <div txt='1'>
-                <div class='loader'><h1>loading...</h1></div>
-                    <div class="content">
-                        <h2 class='emm' >Title&nbsp;</h2>
-                        <p id='title'></p>
-                    </div>
-                    <div id='date'></div>
-                    <div id='author'></div>
-                    <div id='avatar'></div>
+                    <div>
+                        <div class='loader'><h1>loading...</h1></div>
+                        <div class="content">
+                            <h2 class='emm' >Title&nbsp;</h2>
+                            <p id='0'></p>
+                        </div>
+                        <div id='4'></div>
+                        <div id='8'></div>
+                        <div id='12'></div></div>
                 </div>
                 <div txt='1'>
-                <div class='loader'><h1>loading...</h1></div>
+                    <div class='loader'><h1>loading...</h1></div>
                     <div class="content">
                         <h2 class='emm'>Title&nbsp;</h2>
-                        <p id='title2'></p>
+                        <p id='1'></p>
                     </div>
-                    <div id='date2'></div>
-                    <div id='author2'></div>
-                    <div id='avatar2'></div>
+                    <div id='5'></div>
+                    <div id='9'></div>
+                    <div id='13'></div>
                 </div>
                 <div txt='1'>
-                <div class='loader'><h1>loading...</h1></div>
+                    <div class='loader'><h1>loading...</h1></div>
                     <div class="content">
                         <h2 class='emm'>Title&nbsp;</h2>
-                        <p id='title3'></p>
+                        <p id='2'></p>
                     </div>
-                    <div id='date3'></div>
-                    <div id='author3'></div>
-                    <div id='avatar3'></div>
+                    <div id='6'></div>
+                    <div id='10'></div>
+                    <div id='14'></div>
                 </div>
                 <div txt='1'>
-                <div class='loader'><h1>loading...</h1></div>
+                    <div class='loader'><h1>loading...</h1></div>
                     <div class="content">
                         <h2 class='emm'>Title&nbsp;</h2>
-                        <p id='title4'></p>
+                        <p id='3'></p>
                     </div>
-                    <div id='date4'></div>
-                    <div id='author4'></div>
-                    <div id='avatar4'></div>
+                    <div id='7'></div>
+                    <div id='11'></div>
+                    <div id='15'></div>
                 </div>
             </div>
             </Content>
